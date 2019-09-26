dbf9252357c3d721df841cff82431dcc7fcf6b6a
github username <github email address>
2019-09-26 11:06:13 -0400

    all done

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,20 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.create(name: params[:heroine][:name], super_name: params[:heroine][:super_name], power_id: params[:heroine][:power_id])
+    
+    redirect_to heroine_path(@heroine.id)
+  end
+
+  def show  
+    @heroine = Heroine.find(params[:id])
+    @heroine_power = @heroine.heroine_power
+  end
+
 end
--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,8 @@
 class Heroine < ApplicationRecord
+    belongs_to :power
+    validates :super_name, uniqueness: true
+
+    def heroine_power
+        Power.find_by_id(self.power_id)
+    end
 end
