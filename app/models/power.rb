26eb0f0bf7c5abea25532de4edfb51f189af8dcd
github username <github email address>
2019-09-26 10:32:19 -0400

    main challenges done

--- app/models/power.rb
+++ app/models/power.rb
@@ -1,2 +1,7 @@
 class Power < ApplicationRecord
+    has_many :heroines
+
+    def formatted_name
+        self.name.split.map{|word|word.capitalize}.join(" ")
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  resources :powers, only: [:index]
-  resources :heroines, only: [:index]  
+  ###Heroines###
+  get '/heroines', to: 'heroines#index', as: 'heroines'
+  get '/heroines/new', to: 'heroines#new', as: 'new_heroine'
+  get '/heroines/:id', to: 'heroines#show', as: 'heroine'
+
+  post '/heroines', to: 'heroines#create'
+
+  ###Powers###
+  get '/powers', to: 'powers#index', as: 'powers'
+  get '/powers/:id', to: 'powers#show', as: 'power'
 end
